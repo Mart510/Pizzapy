bc85a81c3046616f562fe8eebae0e5db534346a8
Mart510 <martynandrewbillings@gmail.com>
2023-02-25 12:38:14 +0000

    'Pizza menu created'

---
+++ Menu.py
@@ -0,0 +1,51 @@
+#print(
+'''
+┌─────────────────────────────────────────────────────────────────────────────────┐
+│                                   Welcome to                                    │
+│                          _____ _              _____                             │
+│                         |  __ (_)            |  __ \                            │
+│                         | |__) | __________ _| |__) |   _                       │
+│                         |  ___/ |_  /_  / _` |  ___/ | | |                      │
+│                         | |   | |/ / / / (_| | |   | |_| |                      │
+│                         |_|   |_/___/___\__,_|_|    \__, |                      │
+│                                                      __/ |                      │             
+│                                                     |___/                       │
+│                            Fresh Pizza made to order                            │
+│                          ────────────────────────────────                       │
+│                          How it works.                                          │
+│                          1. Choose your pizza size                              │
+│                          2. Choose your sauce(s), and cheese(s)                 │
+│                          3. Pick up to 4 fresh toppings                         │
+│                          4. Place your order                                    │
+│                          Unsure and can't decide?                               │
+│                          See our specials below                                 │   
+│                                                                                 │
+│ ┌──── Pizza size ─────┐ ┌──────── Sauce options ───────┐ ┌── Cheese options ──┐ │
+│ │ Small  9"  = £8.99  │ │ Tomato & basil sauce = £0.50 │ │ Cheddar    = £1.00 │ │
+│ │ Medium 12" = £12.99 │ │ Garlic & herb sauce  = £0.60 │ │ Mozzarella = £2.00 │ │
+│ │ Large  18" = £16.99 │ │ Sweet BBQ sauce      = £0.75 │ │ Pepperjack = £2.00 │ │
+│ │ Chonky 24" = £21.99 │ │ Fire roasted sauce   = £1.00 │ │ Stracciata = £4.00 │ │
+│ └─────────────────────┘ └──────────────────────────────┘ └────────────────────┘ │
+│ ┌─────────────────────────── Toppings choose up to 4 ─────────────────────────┐ │
+│ │ Pepperoni  = £0.50  Sausage   = £0.50  Chorizo   = £0.70  Artichoke = £0.60 │ │
+│ │ Sliced ham = £0.50  Onions    = £0.10  Prawns    = £0.65  Egg       = £0.80 │ │
+│ │ Meatballs  = £0.50  Jalepenos = £0.20  Rocket    = £0.55  Spinach   = £0.30 │ │
+│ │ Bacon      = £0.50  Pineapple = £0.15  Olives    = £0.10  Tomatoes  = £0.30 │ │
+│ │ Chicken    = £0.50  Mushrooms = £0.40  Anchovies = £0.25  Garlic    = £0.40 │ │
+│ └─────────────────────────────────────────────────────────────────────────────┘ │
+│ ┌─── Meatazza ────┐ ┌── Green belt ───┐ ┌─── PySpecial ───┐ ┌─── Mysterios ───┐ │
+│ │ Fire r' sauce   │ │ T & b' sauce    │ │ Fire r' sauce   │ │ Randomiser      │ │
+│ │ Cheddar         │ │ G & h' sauce    │ │ Stracciata      │ │ special pizza   │ │
+│ │ Mozzarella      │ │ Mozzarella      │ │ Mozzarella      │ │ 1 random sauce  │ │
+│ │ Pepperoni       │ │ Mushrooms       │ │ Sausage         │ │ choice          │ │
+│ │ Meatballs       │ │ Olives          │ │ Pepperoni       │ │ 2 random cheese │ │
+│ │ Pepperoni       │ │ Mushrooms       │ │ Sausage         │ │ choices         │ │
+│ │ Bacon           │ │ Rocket          │ │ Chorizo         │ │ 4 random        │ │
+│ │ Chicken         │ │ Artichoke       │ │ Onions          │ │ topping choices │ │
+│ │─────────────────│ │─────────────────│ │─────────────────│ │─────────────────│ │
+│ │ Size + £5.00    │ │ Size + £4.50    │ │ Size + £8.00    │ │ Size + £3.75    │ │
+│ └─────────────────┘ └─────────────────┘ └─────────────────┘ └─────────────────┘ │
+│                                                                                 │
+└─────────────────────────────────────────────────────────────────────────────────┘
+'''
+#)
