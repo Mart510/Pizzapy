903f0edf414fc5369dae1d4ed37590c372b44688
Mart510 <martynandrewbillings@gmail.com>
2023-02-28 21:58:54 +0000

    Added method to calc cost of a special pizza

--- PizzaMaker.py
+++ PizzaMaker.py
@@ -1,3 +1,5 @@
+import random
+
 #To create the Pizza class and control all of it's methods and interactions
 '''
 function will call and create a empty pizza, then successive user inputs
@@ -27,41 +29,101 @@ class Pizza:
                 toppings = self.toppings
                 )   
         )
-    #Method to count order number
-    def counter(self):
-        self.orderNumber += 1
+
+    #Method to let the customer know what they ordered
+    def customerOrder(self):
+        # temp variables to hold everything on the pizza
+
+        #print out the size and then a string with all of the cheese, sauce and topping options
+
+    #Method to calculate cost of a special pizza
+    def specialCost(self):
+        self.price += self.size[1]
+        
+
+    #Method to calculate the cost of custom pizza
+    def cost(self):
+        total = 0
+        #cost of the size
+        total += self.size[1]
+        #cost of the sauce(s)
+        for i in range(self.sauce):
+            total += self.sauce[i][1]
+        #cost of the cheese(s)
+        for i in range(self.cheese):
+            total += self.cheese[i][1]
+        #cost of the topping(s)
+        for i in range(self.toppings):
+            total += self.toppings[i][1]
+        return total
 
     #Special Menu Method
-    def special(self, choice):
+    def specialPizza(self, choice):
         match choice:
             case 'Meatazza':
                 #create a meatazza pizza
-
+                self.sauce.append(pizzaSauce[3])
+                self.cheese.append(pizzaCheese[0])
+                self.cheese.append(pizzaCheese[1])
+                self.toppings.append(pizzaToppings[0])
+                self.toppings.append(pizzaToppings[2])
+                self.toppings.append(pizzaToppings[1])
+                self.toppings.append(pizzaToppings[3])
+                self.toppings.append(pizzaToppings[4])
+                self.price = 5.00
             case 'Green belt':
                 #create a green belt pizza
+                self.sauce.append(pizzaSauce[0])
+                self.sauce.append(pizzaSauce[1])
+                self.cheese.append(pizzaCheese[1])
+                self.toppings.append(pizzaToppings[9])
+                self.toppings.append(pizzaToppings[13])
+                self.toppings.append(pizzaToppings[6])
+                self.toppings.append(pizzaToppings[12])
+                self.toppings.append(pizzaToppings[15])
+                self.price = 4.50
 
             case 'PySpecial':
                 #create a PySpecial
-                
+                self.sauce.append(pizzaSauce[3])
+                self.cheese.append(pizzaCheese[3])
+                self.cheese.append(pizzaCheese[1])
+                self.toppings.append(pizzaToppings[5])
+                self.toppings.append(pizzaToppings[0])
+                self.toppings.append(pizzaToppings[2])
+                self.toppings.append(pizzaToppings[10])
+                self.toppings.append(pizzaToppings[6])
+                self.price = 8.00
+
             case 'Mysterios':
                 #create mysterios pizza
+                self.sauce.append(pizzaSauce[random.randint(0, len(pizzaSauce))])
+                self.cheese.append(pizzaCheese[random.randint(0, len(pizzaCheese))])
+                self.cheese.append(pizzaCheese[random.randint(0, len(pizzaCheese))])
+                self.toppings.append(pizzaToppings[random.randint(0, len(pizzaToppings))])
+                self.toppings.append(pizzaToppings[random.randint(0, len(pizzaToppings))])
+                self.toppings.append(pizzaToppings[random.randint(0, len(pizzaToppings))])
+                self.toppings.append(pizzaToppings[random.randint(0, len(pizzaToppings))])
+                self.price = 3.75
+
+                
 
 
     #Method to update pizza size
     def sizePicker(self, number):
         match number:
             case 1:
-                self.size = pizzaSize[0].append()
-                #self.price += pizzaSize.get('small')
+                self.size = pizzaSize[0]
+                #Size small
             case 2:
-                self.size = pizzaSize[1].append()
-                #self.price += pizzaSize.get('medium')
+                self.size = pizzaSize[1]
+                #Size medium
             case 3:
-                self.size = pizzaSize[2].append()
-                #self.price += pizzaSize.get('large')
+                self.size = pizzaSize[2]
+                #Size large
             case 4:
-                self.size = pizzaSize[3].append()
-                #self.price += pizzaSize.get('chonky')                
+                self.size = pizzaSize[3]
+                #Size chonky                
 
     #Method to update sauce options
     def saucePicker(self, number):
@@ -70,18 +132,14 @@ class Pizza:
                 self.sauce
                 
 
-    #Method to update toppings
-
-    #Method to calculate price
-
 #Pizza variables to be used when creating a pizza
 #pizza sizes
-pizzaSize = [
+pizzaSize = {
     ['small', 8.99], #0
     ['medium', 12.99], #1
     ['large', 16.99], #2
     ['chonky', 21.99] #3
-]
+}
 
 #dictionaries to control all pizza variables
 #pizza sauces
@@ -93,41 +151,39 @@ pizzaSauce = [
 ]
 
 #cheese options
-pizzaCheese = {
-    'cheddar': 1.00,
-    'mozzarella': 2.00,
-    'pepperjack': 2.00,
-    'stracciata': 4.00
-}
+pizzaCheese = [
+    ['cheddar', 1.00,] #0
+    ['mozzarella', 2.00,] #1
+    ['pepperjack', 2.00,] #2
+    ['stracciata', 4.00] #3
+]
 
 #pizza toppings 
-pizzaToppings = {
-    'pepperoni': 0.50,
-    'sliced ham': 0.50,
-    'meatballs': 0.50,
-    'bacon': 0.50,
-    'chicken': 0.50,
-    'sausage': 0.50,
-    'onions': 0.10,
-    'jalepenos': 0.20,
-    'pineapple': 0.15,
-    'mushrooms': 0.40,
-    'chorizo': 0.70,
-    'prawns': 0.65,
-    'rocket': 0.55,
-    'olives': 0.10,
-    'anchoives': 0.25,
-    'artichoke': 0.60,
-    'egg': 0.80,
-    'spinach': 0.30,
-    'tomatoes': 0.30,
-    'garlic': 0.40
-}
-
-#function to create pizza
+pizzaToppings = [
+    ['pepperoni', 0.50], #0
+    ['sliced ham', 0.50], #1
+    ['meatballs', 0.50], #2
+    ['bacon', 0.50], #3
+    ['chicken', 0.50], #4
+    ['sausage', 0.50], #5
+    ['onions', 0.10], #6
+    ['jalepenos', 0.20], #7
+    ['pineapple', 0.15], #8
+    ['mushrooms', 0.40], #9 
+    ['chorizo', 0.70], #10
+    ['prawns', 0.65], #11
+    ['rocket', 0.55], #12
+    ['olives', 0.10], #13
+    ['anchoives', 0.25], #14
+    ['artichoke', 0.60], #15
+    ['egg', 0.80], #16 
+    ['spinach', 0.30], #17
+    ['tomatoes', 0.30], #18
+    ['garlic', 0.40] #19
+]
 
 
-#choosing size
+
 
 
 
--- PizzaPy.py
+++ PizzaPy.py
@@ -38,18 +38,12 @@ def specialOrCustom():
         return 'Custom'
 
 #function to choose pizza size
-def pizzaSizePicker():
-    choice = ''
-    def getInput():
-        choice = input('What size pizza would you like?\n 1. Small, 2. Medium, 3. Large, 4. Chonky, 5. Cancel order')
-        return choice
-    getInput()
-    if int(choice) != 1 or 2 or 3 or 4 or 5:
-        getInput()
-    if int(choice) == 5:
-        return
-    else:
-        return PizzaMaker.sizePicker(choice)
+def choosePizzaSize():
+    #Get their pizza size
+    pizzaSize = 0
+    while pizzaSize != 1 or 2 or 3 or 4:
+        pizzaSize = input('What size would you like?\n 1. Small, 2. Medium, 3. Large, 4. Chonky')
+    PizzaMaker.Pizza.sizePicker(pizzaSize)
 
 #global variables
 orderList = [] #Keeps track of all orders
@@ -61,10 +55,15 @@ def greeter():
     print(
     '''
     Welcome to PizzaPy
-    Please navigate the questions using the numbers on your keyboard
+    Please navigate the questions using the numbers on your keyboard\n
     '''
     )
 
+######################################################
+#program start
+######################################################
+
+greeter()
 
 #intialise pizza
 order = PizzaMaker.Pizza()
@@ -73,8 +72,43 @@ orderList.append(order)
 #ask if they want a special or a custom pizza
 match specialOrCustom():
     case 'Special':
+        choice = ''
         #ask for size then create a special pizza
-        pizzaSizePicker()
+        def getInput():
+            print(
+                '''
+                Today's specials are:
+                The Meatazza, a meat lovers feast
+                The Greenbelt, dedcadence of the forest (vegetarian friendly)
+                The PySpecial, our chef's favourite featuring award winning Stracciata italian cheese
+                The Mysterios, a pizza created at random in a size of your choosing, great for lovers of all things pizza
+                '''
+                )
+            choice = input('1. Meatazza, 2. Greenbelt, 3. PySecial, 4. Mysterios, 5. Cancel order')
+            return choice
+        getInput()
+        #check if input is valid, if not repeat question and give customer option to cancel/break out of loop
+        if int(choice) != 1 or 2 or 3 or 4 or 5:
+            getInput()           
+        if int(choice) == 5:
+
+        match int(choice):
+            case 1:
+                order.specialPizza('Meatazza')
+            case 2:
+                order.specialPizza('Green belt')
+            case 3:
+                order.specialPizza('PySpecial')
+            case 4:
+                PizzaMaker.Pizza.specialPizza('Mysterios')
+            
+        #Set pizza size
+        choosePizzaSize()
+
+        #Let them know what they have ordered and how much, give them a chance to decline the order
+
+        print('Your total is {price}').format(price = order.price)
+
 
     case 'Custom':
         #ask for size and start making a custom pizza
